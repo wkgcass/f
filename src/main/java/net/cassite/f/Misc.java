cf116c5e3df55eb5c939317c3bc9c14ba2816fb1
wkgcass <wkgcass@hotmail.com>
2019-04-24 17:06:28 +0800

    add functional interface annotation

--- src/main/java/net/cassite/f/Misc.java
+++ src/main/java/net/cassite/f/Misc.java
@@ -9,30 +9,37 @@ class Misc {
         int apply(T t);
     }
 
+    @FunctionalInterface
     interface FloatFunction<T> {
         float apply(T t);
     }
 
+    @FunctionalInterface
     interface LongFunction<T> {
         long apply(T t);
     }
 
+    @FunctionalInterface
     interface DoubleFunction<T> {
         double apply(T t);
     }
 
+    @FunctionalInterface
     interface ShortFunction<T> {
         short apply(T t);
     }
 
+    @FunctionalInterface
     interface ByteFunction<T> {
         byte apply(T t);
     }
 
+    @FunctionalInterface
     interface CharFunction<T> {
         char apply(T t);
     }
 
+    @FunctionalInterface
     interface BoolFunction<T> {
         boolean apply(T t);
     }
